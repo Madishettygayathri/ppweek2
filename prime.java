a2480e99feb54d8b1b948812f45a3d86a93d8273
Madishettygayathri <madishettygayathri94@gmail.com>
2024-11-25 10:17:54 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,21 +1,18 @@
-class prime
-{
-    public static void(String[]args)
+class prime{
+    public static void main(String[]args)
     {
-        int n=27,flag=0;
-        for(int i=0;i<math.sqrt(n);i++)
+        int n=23,flag=0;
+        for(int i=2;i<=Math.sqrt(n);i++)
         {
-            if(n%2==0)
+            if(n%i==0)
             {
                 flag=1;
                 break;
             }
         }
-            if(flag==0)
-            System.out.print("yes");
-            else
-            System.out.print("no");
-        
-         
+        if(flag==0)
+        System.out.println("yes");
+        else
+         System.out.println("no");
     }
 }
